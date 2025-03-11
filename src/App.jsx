04672fe3c0dff2746edf1402a4495f0f1c92b111
haderrenteria13 <haderrenteria13az@gmail.com>
2025-03-10 23:29:13 -0500

    Update: modify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import FormEmployees from './components/FormEmployees'
 import ListEmployees from './components/ListEmployees'
 
 function App() {
+  const [saveData, setSaveData] = useState([])
+
+  const sendData = (userData) => {
+    setSaveData([...saveData, userData])
+  }
   return (
     <div>
-      <FormEmployees/>
-      <ListEmployees/>
+      <FormEmployees sendData={sendData}/>
+      <ListEmployees saveData={saveData}/>
     </div>
   )
 }
