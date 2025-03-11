715b46cc0c8b02d321c2bbd0c81bbba39928c635
haderrenteria13 <haderrenteria13az@gmail.com>
2025-03-10 22:54:36 -0500

    Feat: create events and Hooks for FormEmployees

--- src/components/FormEmployees.jsx
+++ src/components/FormEmployees.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormEmployees() {
+    const [document, setDocument] = useState ('')
+    const [name, setName] = useState ('')
+    const [address, setAddress] = useState ('')
+    const [phone, setPhone] = useState ('')
+    const [status, setStatus] = useState (true)
+
+    const addData = (e) => {
+        e.preventDefault()
+        sendData ({document, name, address, phone, status})
+        setDocument('')
+        setName('')
+        setAddress('')
+        setPhone('')
+    }
+
     return (
-        <form>
+        <form  onSubmit={addData}>
             <div>
-                <label htmlFor="documento">Documento:</label>
-                <input type="number" />
+                <label htmlFor="document">Documento:</label>
+                <input type="number" name="document" onChange={(e) => setDocument(e.target.value)} value={document} />
             </div>
             <div>
                 <label htmlFor="name">Nombre:</label>
-                <input type="text" />
+                <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name}/>
             </div>
             <div>
                 <label htmlFor="address">Dirección:</label>
-                <input type="text" />
+                <input type="text" name="address" onChange={(e) => setAddress(e.target.value)} value={address}/>
             </div>
             <div>
                 <label htmlFor="phone">Telefono:</label>
-                <input type="number" />
+                <input type="number" name="phone" onChange={(e) => setPhone(e.target.value)} value={phone}/>
             </div>
             <div>
                 <label htmlFor="status">Estado:</label>
-                <select name="status" id="status-select">
+                <select name="status" onChange={(e) => setStatus(e.target.value)} value={status} >
                     <option value="active">Activo</option>
                     <option value="inactive">Inactivo</option>
                 </select>
             </div>
+            <button type="submit">Guardar</button>
         </form>
     )
 }
